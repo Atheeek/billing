86e805302aa9746ab2277e0e568e33a0b6dc3ac7
Atheeek <atheek163@gmail.com>
2025-05-03 17:50:54 +0530

    Add CORS config and use env for MongoDB URI

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // CORS configuration to allow Vercel frontend
 app.use(cors({
-  origin: 'https://billing-8jwe90824-atheeks-projects-bad38512.vercel.app', // ✅ Replace with your actual Vercel domain
+  origin: 'https://billing-jdoefb8vf-atheeks-projects-bad38512.vercel.app', // ✅ Replace with your actual Vercel domain
   credentials: true
 }));
 
